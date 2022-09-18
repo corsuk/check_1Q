51b2c4482572a641088328c99b5df029fd24d590
corsuk <irina.abagaeva@gmail.com>
2022-09-19 00:27:49 +0300

    task

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿void ThreeSymbols (string [] array)
+{ 
+    int count = 0;
+    string [] arr = new string [array.Length];
+    for (int i = 0; i < array.Length-1; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            arr[count] =array[i];
+            count++;
+        }
+    }
+    PrintArr(arr, count);
+}
+void PrintArr (string [] arr, int count)
+{
+    for ( int i = 0; i < count; i++)
+    {
+        Console.Write(arr[i]+ ", ");
+    }
+}
+
+string [] array1 = {"1234", "1567", "-2", "computer science"};
+Console.WriteLine(string.Join("; ", array1));
+ThreeSymbols(array1);
